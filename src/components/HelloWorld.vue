babf54ae69dee608af06e071b4975db1dd3cb9c0
Devin Lethbridge <devinbeth@gmail.com>
2018-03-29 13:40:12 -0600

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,67 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <input placeholder="Enter nerdy thing" v-model="currentThing" v-on:keydown.enter="updateCurrentThing" />
+    <br/>
+    {{ currentThing }}
+    <br/>
+    <div>
+      <h2>To Experience</h2>
+      <div v-for="thing in toExperience" :key="thing.name"
+        @click="markCompleted(thing)"
+        :class="{
+          completed: thing.completed
+        }"
+      >
+        {{ thing.name }}
+      </div>
+      <h2>Experienced</h2>
+      <div v-for="thing in experienced" :key="thing.name"
+        @click="markCompleted(thing)"
+        :class="{
+          completed: thing.completed
+        }"
+      >
+        {{ thing.name }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      exclamation: "!!!",
+      nerdStuff: [],
+      currentThing: ""
+    };
+  },
+  computed: {
+    toExperience: function() {
+      return this.nerdStuff.filter(item => !item.completed);
+    },
+    experienced: function() {
+      return this.nerdStuff.filter(item => item.completed);
+    }
+  },
+  methods: {
+    updateCurrentThing: function() {
+      this.nerdStuff.push({
+        name: this.currentThing,
+        completed: false
+      });
+      this.currentThing = '';
+    },
+    markCompleted(thing) {
+      thing.completed = !thing.completed;
+      console.log(thing);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -53,5 +79,9 @@ li {
 }
 a {
   color: #42b983;
+}
+.completed {
+  color: lightgrey;
+  text-decoration: line-through;
 }
 </style>
